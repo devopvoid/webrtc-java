a504bcb01d22762c0633375b1add12385fdc9a4f
Alex Andres <andres.alex@protonmail.com>
2020-01-06 04:19:23 +0100

    Added module-infos and changed JSON api dependencies

---
+++ webrtc-demo/webrtc-demo-api/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module webrtc.java.demo.api {
+
+	requires com.google.guice;
+
+	requires java.json;
+	requires java.json.bind;
+	requires java.logging;
+	requires java.net.http;
+	requires javax.inject;
+
+	requires webrtc.java;
+
+}
---
+++ webrtc/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module webrtc.java {
+
+	requires java.desktop;
+
+	exports dev.onvoid.webrtc;
+	exports dev.onvoid.webrtc.logging;
+	exports dev.onvoid.webrtc.media;
+	exports dev.onvoid.webrtc.media.audio;
+	exports dev.onvoid.webrtc.media.video;
+	exports dev.onvoid.webrtc.media.video.desktop;
+
+}
