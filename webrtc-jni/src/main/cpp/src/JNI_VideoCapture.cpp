eec4cfdb283da18b41476555d49de925a6fa42ce
Alex Andres <andres.alex@protonmail.com>
2021-05-17 21:53:04 +0200

    Add VideoCapture to capture frames without a running track

---
+++ webrtc-jni/src/main/cpp/src/JNI_VideoCapture.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2021 Alex Andres
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "JNI_VideoCapture.h"
+#include "api/VideoTrackSink.h"
+#include "media/video/VideoCapture.h"
+#include "media/video/VideoDevice.h"
+#include "JavaRef.h"
+#include "JavaObject.h"
+#include "JavaString.h"
+#include "JavaUtils.h"
+
+JNIEXPORT void JNICALL Java_dev_onvoid_webrtc_media_video_VideoCapture_setVideoCaptureDevice
+(JNIEnv * env, jobject caller, jobject device)
+{
+	if (!device) {
+		env->Throw(jni::JavaNullPointerException(env, "VideoDevice is null"));
+		return;
+	}
+
+	jni::VideoCapture * videoSource = GetHandle<jni::VideoCapture>(env, caller);
+	CHECK_HANDLE(videoSource);
+
+	const auto javaClass = jni::JavaClasses::get<jni::VideoDevice::JavaVideoDeviceClass>(env);
+	const auto dev = jni::VideoDevice::toNativeVideoDevice(env, jni::JavaLocalRef<jobject>(env, device));
+
+	videoSource->setDevice(std::make_shared<jni::avdev::VideoDevice>(dev.getName(), dev.getDescriptor()));
+}
+
+JNIEXPORT void JNICALL Java_dev_onvoid_webrtc_media_video_VideoCapture_setVideoCaptureCapability
+(JNIEnv * env, jobject caller, jobject jcapability)
+{
+	jni::VideoCapture * videoSource = GetHandle<jni::VideoCapture>(env, caller);
+	CHECK_HANDLE(videoSource);
+
+	if (!jcapability) {
+		env->Throw(jni::JavaNullPointerException(env, "VideoCaptureCapability is null"));
+		return;
+	}
+
+	jint width = env->GetIntField(jcapability, GetFieldID(env, jcapability, "width", "I"));
+	jint height = env->GetIntField(jcapability, GetFieldID(env, jcapability, "height", "I"));
+	jint frameRate = env->GetIntField(jcapability, GetFieldID(env, jcapability, "frameRate", "I"));
+
+	webrtc::VideoCaptureCapability capability;
+	capability.width = static_cast<int32_t>(width);
+	capability.height = static_cast<int32_t>(height);
+	capability.maxFPS = static_cast<int32_t>(frameRate);
+
+	videoSource->setVideoCaptureCapability(capability);
+}
+
+JNIEXPORT void JNICALL Java_dev_onvoid_webrtc_media_video_VideoCapture_setVideoSink
+(JNIEnv * env, jobject caller, jobject jsink)
+{
+	if (jsink == nullptr) {
+		env->Throw(jni::JavaNullPointerException(env, "VideoTrackSink must not be null"));
+		return;
+	}
+
+	jni::VideoCapture * videoSource = GetHandle<jni::VideoCapture>(env, caller);
+	CHECK_HANDLE(videoSource);
+
+	videoSource->setVideoSink(std::make_unique<jni::VideoTrackSink>(env, jni::JavaGlobalRef<jobject>(env, jsink)));
+}
+
+JNIEXPORT void JNICALL Java_dev_onvoid_webrtc_media_video_VideoCapture_start
+(JNIEnv * env, jobject caller)
+{
+	jni::VideoCapture * videoSource = GetHandle<jni::VideoCapture>(env, caller);
+	CHECK_HANDLE(videoSource);
+
+	try {
+		videoSource->start();
+	}
+	catch (...) {
+		ThrowCxxJavaException(env);
+	}
+}
+
+JNIEXPORT void JNICALL Java_dev_onvoid_webrtc_media_video_VideoCapture_stop
+(JNIEnv * env, jobject caller)
+{
+	jni::VideoCapture * videoSource = GetHandle<jni::VideoCapture>(env, caller);
+	CHECK_HANDLE(videoSource);
+
+	try {
+		videoSource->stop();
+	}
+	catch (...) {
+		ThrowCxxJavaException(env);
+	}
+}
+
+JNIEXPORT void JNICALL Java_dev_onvoid_webrtc_media_video_VideoCapture_dispose
+(JNIEnv * env, jobject caller)
+{
+	jni::VideoCapture * videoSource = GetHandle<jni::VideoCapture>(env, caller);
+	CHECK_HANDLE(videoSource);
+
+	SetHandle<std::nullptr_t>(env, caller, nullptr);
+
+	videoSource = nullptr;
+}
+
+JNIEXPORT void JNICALL Java_dev_onvoid_webrtc_media_video_VideoCapture_initialize
+(JNIEnv * env, jobject caller)
+{
+	SetHandle(env, caller, new jni::VideoCapture());
+}
---
+++ webrtc-jni/src/main/cpp/src/media/video/VideoCapture.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2021 Alex Andres
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "media/video/VideoCapture.h"
+#include "Exception.h"
+
+#include "api/video/i420_buffer.h"
+#include "modules/video_capture/video_capture_factory.h"
+
+namespace jni
+{
+	VideoCapture::VideoCapture() :
+		captureModule(nullptr)
+	{
+		capability.width = static_cast<int32_t>(1280);
+		capability.height = static_cast<int32_t>(720);
+		capability.maxFPS = static_cast<int32_t>(30);
+	}
+
+	VideoCapture::~VideoCapture()
+	{
+		destroy();
+	}
+
+	void VideoCapture::setDevice(const avdev::DevicePtr & device)
+	{
+		this->device = device;
+	}
+
+	void VideoCapture::setVideoCaptureCapability(const webrtc::VideoCaptureCapability & capability)
+	{
+		this->capability = capability;
+	}
+
+	void VideoCapture::setVideoSink(std::unique_ptr<rtc::VideoSinkInterface<webrtc::VideoFrame>> sink)
+	{
+		this->sink = std::move(sink);
+	}
+
+	void VideoCapture::start()
+	{
+		if (!device) {
+			throw new Exception("Video device must be set");
+		}
+
+		std::unique_ptr<webrtc::VideoCaptureModule::DeviceInfo> info(webrtc::VideoCaptureFactory::CreateDeviceInfo());
+
+		if (!info) {
+			throw new Exception("Create video DeviceInfo failed");
+		}
+
+		uint32_t num = info->NumberOfDevices();
+
+		if (num < 1) {
+			throw new Exception("No video capture devices available");
+		}
+		
+		std::string devUid;
+		const uint32_t size = webrtc::kVideoCaptureDeviceNameLength;
+
+		for (uint32_t i = 0; i < num; ++i) {
+			char name[size] = { 0 };
+			char guid[size] = { 0 };
+
+			int32_t ret = info->GetDeviceName(i, name, size, guid, size);
+
+			if (ret != 0) {
+				RTC_LOG(WARNING) << "Get video capture device name failed";
+				continue;
+			}
+
+			if (device->getName().compare(name) == 0) {
+				devUid = guid;
+				break;
+			}
+		}
+
+		if (devUid.empty()) {
+			throw new Exception("Device %s not found", device->getName().c_str());
+		}
+
+		captureModule = webrtc::VideoCaptureFactory::Create(devUid.c_str());
+
+		if (!captureModule) {
+			throw new Exception("Create VideoCaptureModule for UID %s failed", devUid.c_str());
+		}
+
+		captureModule->RegisterCaptureDataCallback(std::move(sink).release());
+
+		capability.videoType = webrtc::VideoType::kI420;
+
+		if (captureModule->StartCapture(capability) != 0) {
+			destroy();
+
+			throw new Exception("Start video capture for UID %s failed", devUid.c_str());
+		}
+
+		if (!captureModule || !captureModule->CaptureStarted()) {
+			throw new Exception("Start video capture failed");
+		}
+	}
+
+	void VideoCapture::stop()
+	{
+		destroy();
+	}
+
+	void VideoCapture::destroy()
+	{
+		if (!captureModule) {
+			return;
+		}
+		if (captureModule->CaptureStarted()) {
+			captureModule->StopCapture();
+		}
+
+		captureModule->DeRegisterCaptureDataCallback();
+		captureModule = nullptr;
+	}
+}
