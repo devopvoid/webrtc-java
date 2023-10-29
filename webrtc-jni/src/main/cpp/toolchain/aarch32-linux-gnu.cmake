7d3996ce6d0723b738975fbf4bc5b4fc0d5eb465
Alex Andres <andres.alex@protonmail.com>
2023-10-29 22:15:29 +0100

    Added Linux ARM architectures for cross compilation

---
+++ webrtc-jni/src/main/cpp/toolchain/aarch32-linux-gnu.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME		Linux)
+set(CMAKE_SYSTEM_PROCESSOR	arm)
+set(CMAKE_C_COMPILER		/usr/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER		/usr/bin/arm-linux-gnueabihf-g++)
+
+set(TARGET_CPU				"arm")
+
+set(SYSROOT_ARCH			"arm")
+set(SYSROOT_PATH			"src/build/linux/debian_sid_arm-sysroot")
---
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-linux-gnu.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME		Linux)
+set(CMAKE_SYSTEM_PROCESSOR	x86_64)
+
+set(TARGET_CPU				"x64")
+
+set(SYSROOT_ARCH			"amd64")
+set(SYSROOT_PATH			"src/build/linux/debian_sid_amd64-sysroot")
