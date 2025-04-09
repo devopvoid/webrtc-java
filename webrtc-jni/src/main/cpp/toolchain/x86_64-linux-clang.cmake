f03dab948416f25b706423972feb2a6cbd20d5e8
Alex Andres <andres.alex@protonmail.com>
2025-04-09 10:56:06 +0200

    fix: build and link with libc++ and libc++abi

--- webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
@@ -1,6 +1,8 @@
 set(CMAKE_SYSTEM_NAME		Linux)
 set(CMAKE_SYSTEM_PROCESSOR	x86_64)
-set(CMAKE_C_COMPILER		/usr/bin/clang-21)
-set(CMAKE_CXX_COMPILER		/usr/bin/clang++-21)
+set(CMAKE_C_COMPILER		/home/alex/webrtc/src/third_party/llvm-build/Release+Asserts/bin/clang)
+set(CMAKE_CXX_COMPILER		/home/alex/webrtc/src/third_party/llvm-build/Release+Asserts/bin/clang++)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -nostdinc++ -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE")
 
 set(TARGET_CPU				"x64")
