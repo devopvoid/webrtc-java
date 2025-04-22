96ade05578c78a84418bdff2dff42c73e9ac53e7
Alex Andres <andres.alex@protonmail.com>
2025-04-22 13:45:53 +0200

    refactor: on linux link against libc++ and libc++abi

--- webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
@@ -1,8 +1,13 @@
 set(CMAKE_SYSTEM_NAME		Linux)
 set(CMAKE_SYSTEM_PROCESSOR	x86_64)
-set(CMAKE_C_COMPILER		/home/alex/webrtc/src/third_party/llvm-build/Release+Asserts/bin/clang)
-set(CMAKE_CXX_COMPILER		/home/alex/webrtc/src/third_party/llvm-build/Release+Asserts/bin/clang++)
+set(CMAKE_C_COMPILER		/tmp/clang/bin/clang)
+set(CMAKE_CXX_COMPILER		/tmp/clang/bin/clang++)
+set(CMAKE_AR		        /tmp/clang/bin/llvm-ar)
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -nostdinc++ -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE")
+
+foreach(LINKER SHARED_LINKER)
+    set(CMAKE_${LINKER}_FLAGS "-fuse-ld=lld -Wl,-s")
+endforeach()
 
 set(TARGET_CPU				"x64")
