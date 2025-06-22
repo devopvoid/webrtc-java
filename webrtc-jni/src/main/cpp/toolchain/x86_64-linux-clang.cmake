114351ff9cb8586bd628b735e25fb5d95648590c
Alex Andres <andres.alex@protonmail.com>
2025-06-23 01:54:11 +0200

    build: update clang compiler paths in x86_64-linux-clang.cmake

--- webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_NAME		Linux)
 set(CMAKE_SYSTEM_PROCESSOR	x86_64)
-set(CMAKE_C_COMPILER		/tmp/clang/bin/clang)
-set(CMAKE_CXX_COMPILER		/tmp/clang/bin/clang++)
-set(CMAKE_AR		        /tmp/clang/bin/llvm-ar)
+set(CMAKE_C_COMPILER		/opt/clang/bin/clang)
+set(CMAKE_CXX_COMPILER		/opt/clang/bin/clang++)
+set(CMAKE_AR		        /opt/clang/bin/llvm-ar)
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -nostdinc++ -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE")
 
