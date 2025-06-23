3d86636f66355ebe34a11c453bb6f08dab592847
Alex Andres <andres.alex@protonmail.com>
2025-06-23 12:42:26 +0200

    build: add libc++ as the standard library for linking

--- webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
@@ -4,7 +4,8 @@ set(CMAKE_C_COMPILER		/opt/clang/bin/clang)
 set(CMAKE_CXX_COMPILER		/opt/clang/bin/clang++)
 set(CMAKE_AR		        /opt/clang/bin/llvm-ar)
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -nostdinc++ -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE")
+set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} -nostdinc++ -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE")
+set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++")
 
 foreach(LINKER SHARED_LINKER)
     set(CMAKE_${LINKER}_FLAGS "-fuse-ld=lld -Wl,-s")
