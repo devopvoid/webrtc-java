e754b1eb1813378d65f5aae0272e38c0a250f6e1
Alex Andres <andres.alex@protonmail.com>
2025-07-01 17:38:23 +0200

    build: update CMake and action files for aarch32 and aarch64 support

---
+++ webrtc-jni/src/main/cpp/toolchain/aarch64-linux-clang.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_SYSTEM_NAME		Linux)
+set(CMAKE_SYSTEM_PROCESSOR	aarch64)
+set(CMAKE_C_COMPILER		/opt/clang/bin/clang)
+set(CMAKE_CXX_COMPILER		/opt/clang/bin/clang++)
+set(CMAKE_AR		        /opt/clang/bin/llvm-ar)
+
+set(TARGET_TRIPLE           aarch64-linux-gnu)
+
+set(CMAKE_C_FLAGS           "--target=${TARGET_TRIPLE}")
+set(CMAKE_CXX_FLAGS         "${CMAKE_C_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -v -Wl,--verbose")
+
+foreach(LINKER SHARED_LINKER)
+    set(CMAKE_${LINKER}_FLAGS "-fuse-ld=lld -Wl,-s -v -Wl,--verbose")
+endforeach()
+
+set(TARGET_CPU				"arm64")
