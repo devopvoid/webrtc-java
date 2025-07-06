930cd70781334e4bd3cb5bf9376cfb3356e2b9f5
Alex Andres <58339654+devopvoid@users.noreply.github.com>
2025-07-07 01:05:11 +0200

    build: update package installation commands and enhance sysroot configuration (#178)

--- webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
@@ -1,8 +1,8 @@
-set(CMAKE_SYSTEM_NAME		Linux)
-set(CMAKE_SYSTEM_PROCESSOR	x86_64)
-set(CMAKE_C_COMPILER		/opt/clang/bin/clang)
-set(CMAKE_CXX_COMPILER		/opt/clang/bin/clang++)
-set(CMAKE_AR		        /opt/clang/bin/llvm-ar)
+set(CMAKE_SYSTEM_NAME       Linux)
+set(CMAKE_SYSTEM_PROCESSOR  x86_64)
+set(CMAKE_C_COMPILER        /opt/clang/bin/clang)
+set(CMAKE_CXX_COMPILER      /opt/clang/bin/clang++)
+set(CMAKE_AR                /opt/clang/bin/llvm-ar)
 
 set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} -nostdinc++ -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE")
 set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -v -Wl,--verbose")
@@ -11,4 +11,14 @@ foreach(LINKER SHARED_LINKER)
     set(CMAKE_${LINKER}_FLAGS "-fuse-ld=lld -Wl,-s -v -Wl,--verbose")
 endforeach()
 
-set(TARGET_CPU				"x64")
+# IMPORTANT: Find sysroot but DO NOT set CMAKE_SYSROOT yet
+file(GLOB LINUX_SYSROOT "/opt/sysroot/debian*amd64*")
+if(NOT LINUX_SYSROOT)
+    message(FATAL_ERROR "No debian amd64 sysroot found in /opt/sysroot/")
+endif()
+
+# Store sysroot path for later application
+# This will be set after project() call in the main CMakeLists.txt
+set(DEFERRED_SYSROOT ${LINUX_SYSROOT})
+
+set(TARGET_CPU              "x64")
