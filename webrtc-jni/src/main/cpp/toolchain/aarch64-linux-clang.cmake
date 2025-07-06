76073a2cc5f7e33fc99452323e32e0d5b90c97b2
Alex Andres <andres.alex@protonmail.com>
2025-07-06 12:38:15 +0200

    build: defer sysroot configuration for linux

--- webrtc-jni/src/main/cpp/toolchain/aarch64-linux-clang.cmake
+++ webrtc-jni/src/main/cpp/toolchain/aarch64-linux-clang.cmake
@@ -1,8 +1,8 @@
-set(CMAKE_SYSTEM_NAME		Linux)
-set(CMAKE_SYSTEM_PROCESSOR	aarch64)
-set(CMAKE_C_COMPILER		/opt/clang/bin/clang)
-set(CMAKE_CXX_COMPILER		/opt/clang/bin/clang++)
-set(CMAKE_AR		        /opt/clang/bin/llvm-ar)
+set(CMAKE_SYSTEM_NAME       Linux)
+set(CMAKE_SYSTEM_PROCESSOR  aarch64)
+set(CMAKE_C_COMPILER        /opt/clang/bin/clang)
+set(CMAKE_CXX_COMPILER      /opt/clang/bin/clang++)
+set(CMAKE_AR                /opt/clang/bin/llvm-ar)
 
 set(TARGET_TRIPLE           aarch64-linux-gnu)
 
@@ -14,16 +14,14 @@ foreach(LINKER SHARED_LINKER)
     set(CMAKE_${LINKER}_FLAGS "-fuse-ld=lld -Wl,-s -v -Wl,--verbose")
 endforeach()
 
+# IMPORTANT: Find sysroot but DO NOT set CMAKE_SYSROOT yet
 file(GLOB LINUX_SYSROOT "/opt/sysroot/debian*arm64*")
 if(NOT LINUX_SYSROOT)
     message(FATAL_ERROR "No debian arm64 sysroot found in /opt/sysroot/")
 endif()
 
-set(CMAKE_SYSROOT ${LINUX_SYSROOT})
+# Store sysroot path for later application
+# This will be set after project() call in the main CMakeLists.txt
+set(DEFERRED_SYSROOT ${LINUX_SYSROOT})
 
-# Make sure CMake finds libraries and headers in the sysroot
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-set(TARGET_CPU				"arm64")
+set(TARGET_CPU              "arm64")
--- webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-linux-clang.cmake
@@ -1,8 +1,8 @@
-set(CMAKE_SYSTEM_NAME		Linux)
-set(CMAKE_SYSTEM_PROCESSOR	x86_64)
-set(CMAKE_C_COMPILER		/opt/clang/bin/clang)
-set(CMAKE_CXX_COMPILER		/opt/clang/bin/clang++)
-set(CMAKE_AR		        /opt/clang/bin/llvm-ar)
+set(CMAKE_SYSTEM_NAME       Linux)
+set(CMAKE_SYSTEM_PROCESSOR  x86_64)
+set(CMAKE_C_COMPILER        /opt/clang/bin/clang)
+set(CMAKE_CXX_COMPILER      /opt/clang/bin/clang++)
+set(CMAKE_AR                /opt/clang/bin/llvm-ar)
 
 set(CMAKE_CXX_FLAGS         "${CMAKE_CXX_FLAGS} -nostdinc++ -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_EXTENSIVE")
 set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -v -Wl,--verbose")
@@ -11,16 +11,14 @@ foreach(LINKER SHARED_LINKER)
     set(CMAKE_${LINKER}_FLAGS "-fuse-ld=lld -Wl,-s -v -Wl,--verbose")
 endforeach()
 
+# IMPORTANT: Find sysroot but DO NOT set CMAKE_SYSROOT yet
 file(GLOB LINUX_SYSROOT "/opt/sysroot/debian*amd64*")
 if(NOT LINUX_SYSROOT)
     message(FATAL_ERROR "No debian amd64 sysroot found in /opt/sysroot/")
 endif()
 
-set(CMAKE_SYSROOT ${LINUX_SYSROOT})
+# Store sysroot path for later application
+# This will be set after project() call in the main CMakeLists.txt
+set(DEFERRED_SYSROOT ${LINUX_SYSROOT})
 
-# Make sure CMake finds libraries and headers in the sysroot
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-set(TARGET_CPU				"x64")
+set(TARGET_CPU              "x64")
