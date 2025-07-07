43a18fbdedfbbd25ca53ad7ca7aa5ae2e4296f36
Alex Andres <andres.alex@protonmail.com>
2025-07-07 19:39:29 +0200

    build: update macOS build configurations

---
+++ webrtc-jni/src/main/cpp/toolchain/x86_64-macos-cross.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_SYSTEM_NAME                  Darwin)
+set(CMAKE_SYSTEM_PROCESSOR             x86_64)
+
+set(CMAKE_OSX_ARCHITECTURES            x86_64)
+set(CMAKE_OSX_DEPLOYMENT_TARGET        "10.15")
+
+set(CMAKE_C_FLAGS_INIT                 "-arch x86_64")
+set(CMAKE_CXX_FLAGS_INIT               "-arch x86_64")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT        "-arch x86_64")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT     "-arch x86_64")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM  NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY  ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE  ONLY)
+
+set(TARGET_CPU                         "x64")
