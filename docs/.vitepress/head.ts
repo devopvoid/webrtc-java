64e80f93ce27300bd26b4a9575bf2aba8cd437c5
Alex Andres <andres.alex@protonmail.com>
2025-09-09 19:12:15 +0200

    docs: update head config

--- docs/.vitepress/head.ts
+++ docs/.vitepress/head.ts
@@ -1,24 +1,27 @@
 import { HeadConfig } from "vitepress/types/shared";
 
-export const head: HeadConfig[] = [
-	['link', { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png"}],
-	['link', { rel: "icon", type: "image/svg+xml", href: "/favicon.svg"}],
-	['link', { rel: "shortcut icon", href: "/favicon.ico"}],
-	['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"}],
-	['link', { rel: "manifest", href: "/site.webmanifest"}],
-	['meta', { name: "apple-mobile-web-app-title", content: "JRTC"}],
+const isProd = process.env.NODE_ENV === 'production';
 
-	// Security headers
-	[
-		'meta',
-		{
-			'http-equiv': 'Content-Security-Policy',
-			content:
-				"default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:;",
-		},
-	],
-	['meta', { 'http-equiv': 'X-Content-Type-Options', content: 'nosniff' }],
-	['meta', { 'http-equiv': 'X-Frame-Options', content: 'SAMEORIGIN' }],
-	['meta', { 'http-equiv': 'X-XSS-Protection', content: '1; mode=block' }],
-	['meta', { name: 'referrer', content: 'strict-origin-when-cross-origin' }],
+const head: HeadConfig[] = [
+	['link', { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png" }],
+	['link', { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+	['link', { rel: "shortcut icon", href: "/favicon.ico" }],
+	['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" }],
+	['link', { rel: "manifest", href: "/site.webmanifest" }],
+	['meta', { name: "apple-mobile-web-app-title", content: "JRTC" }],
 ]
+
+if (isProd) {
+	head.push([
+		'script',
+		{
+			async: '',
+			src: 'https://eu.umami.is/script.js',
+			'data-website-id': '126654a9-5f07-4b57-ad7e-023eda3980ff',
+			'data-domains': 'jrtc.dev',
+			'data-do-not-track': 'true'
+		},
+	])
+}
+
+export { head }
